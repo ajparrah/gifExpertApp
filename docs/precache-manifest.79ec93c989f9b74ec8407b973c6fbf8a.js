dd3215b1f26fbedd4c3a7bb2227e2de924822411
ajparrah <alejandro_parra_15@hotmail.com>
2020-10-18 10:23:38 -0400

    :package: Added docs to deploy

---
+++ docs/precache-manifest.79ec93c989f9b74ec8407b973c6fbf8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589308403847763101d36e279db53757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78590908110f5f241935",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "f1a34a5fb0e14c5dc512",
+    "url": "/static/css/main.cba06066.chunk.css"
+  },
+  {
+    "revision": "78590908110f5f241935",
+    "url": "/static/js/2.3915849e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3915849e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a34a5fb0e14c5dc512",
+    "url": "/static/js/main.cc8c68ba.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
